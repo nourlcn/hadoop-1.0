f4ba98ed0989891bb761706c9e5554330e1f08af
Nourl <nourlcn@gmail.com>
2012-05-25 13:40:44 +0800

    modify export.sh, add rm name and data,  for test new hadoop well

--- export.sh
+++ export.sh
@@ -1,16 +1,25 @@
 export HADOOP_HOME=/home/nourl/github/hadoop-common-1.0/build/hadoop-1.0.4-SNAPSHOT
 export JAVA_HOME=/usr/lib/jvm/java-6-sun
 
+current=`pwd`
+
 cp ./myconf/* ./build/hadoop-1.0.4-SNAPSHOT/conf/
 
 cd ./build/hadoop-1.0.4-SNAPSHOT/
 
+rm /home/hadoop/hdfs/name -r
+rm /home/hadoop/hdfs/data -r
+
 ./bin/hadoop namenode -format
 
-./bin/hadoop namenode 
+./bin/start-all.sh
 
-./bin/hadoop datanode
+./bin/hadoop jar hadoop-examples-1.0.4-SNAPSHOT.jar pi 1 1
 
-./bin/hadoop jobtracker
+cd $current
 
-./bin/hadoop tasktracker
+echo "./bin/hadoop jar hadoop-examples-1.0.4-SNAPSHOT.jar wordcount /input/in /output/1"
+#./bin/hadoop namenode 
+#./bin/hadoop datanode
+#./bin/hadoop jobtracker
+#./bin/hadoop tasktracker
